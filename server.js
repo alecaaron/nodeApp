4aeeed69758125ad2908cb2271fb26e54f77a0cb
Alec <alecalegria@gmail.com>
2018-03-16 23:16:10 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,48 @@
+let Express = require('express');
+let App = Express();
+let _http = require('http').Server(App);
+let io = require('socket.io')(_http);
+let mongoose = require('mongoose');
+let BodyParser = require('body-parser');
+
+App.use(Express.static(__dirname));
+App.use(BodyParser.json());
+App.use(BodyParser.urlencoded({extended:false}));
+
+let dbUrl = 'mongodb://admin:password@ds147668.mlab.com:47668/nodechatapp';
+
+let Message = mongoose.model('message', {
+  name: String,
+  message: String
+})
+
+App.get('/messages', (req,res) => {
+    Message.find({},(err, messages)=>{
+      res.send(messages);
+    });
+});
+
+App.post('/messages', (req,res) =>{
+  let message = new Message(req.body);
+
+  message.save((err)=>{
+    if(err){
+      sendStatus(500);
+    }else{
+      io.emit('message', req.body);
+      res.sendStatus(200);
+    }
+  });
+});
+
+io.on('connection', socket =>{
+  console.log('A new user connected');
+});
+
+mongoose.connect(dbUrl, err =>{
+  console.log('mongodb connected', err)
+});
+
+const Server = _http.listen(3000, () => {
+  console.log('Server is on port', Server.address().port)
+});
